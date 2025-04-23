1711130e6d98f3a42eab67a9f3765143898f8cfa
Shin <vanvietkhtn@gmail.com>
2025-04-24 00:35:37 +0700

    add Home page

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -10,7 +10,7 @@ export default function Layout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <div className="flex min-h-screen w-full flex-col relative">
-      <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+      <header className="sticky z-20 top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
         <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:text-sm md:gap-5 lg:gap-6">
           <Link
             href={'#'}
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,7 +1,65 @@
+import dishApiRequest from '@/apiRequests/dish'
+import { formatCurrency } from '@/lib/utils'
+import { DishListResType } from '@/schemaValidations/dish.schema'
 import Image from 'next/image'
 
-export default function Home() {
+export default async function Home() {
+  let dishList: DishListResType['data'] = []
+  try {
+    const result = await dishApiRequest.list()
+    const {
+      payload: { data },
+    } = result
+    dishList = data
+  } catch (error) {
+    return <div>Something went wrong</div>
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <div className="w-full space-y-4">
+      <section className="relative z-10">
+        <span className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-10"></span>
+        <Image
+          src="/banner.png"
+          width={400}
+          height={400}
+          quality={100}
+          alt="Banner"
+          className="absolute top-0 left-0 w-full h-full object-cover"
+        />
+        <div className="z-20 relative py-10 md:py-20 px-4 sm:px-10 md:px-20">
+          <h1 className="text-center text-xl sm:text-2xl md:text-4xl lg:text-5xl font-bold">
+            Nhà hàng Beef Restaurant
+          </h1>
+          <p className="text-center text-sm sm:text-base mt-4">
+            Chào mừng bạn đến với Beef Restaurant, nơi mang đến những trải
+            nghiệm ẩm thực tuyệt vời nhất!
+          </p>
+        </div>
+      </section>
+      <section className="space-y-10 py-16">
+        <h2 className="text-center text-2xl font-bold">Đa dạng các món ăn</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+          {dishList.map((dish) => (
+            <div className="flex gap-4" key={dish.id}>
+              <div className="flex-shrink-0">
+                <Image
+                  src={dish.image}
+                  width={150}
+                  height={150}
+                  quality={100}
+                  alt={dish.name}
+                  className="w-[150px] h-[150px] object-cover"
+                />
+              </div>
+              <div className="space-y-1">
+                <h3 className="text-xl font-semibold">{dish.name}</h3>
+                <p className="">{dish.description}</p>
+                <p className="font-semibold">{formatCurrency(dish.price)}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
   )
 }
