35c6ad3753845ccbd445a1e1eff617018ff91f30
Shin <vanvietkhtn@gmail.com>
2025-04-26 08:45:40 +0700

    UI: implement UI guest menu

--- src/app/guest/menu/page.tsx
+++ src/app/guest/menu/page.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import MenuOrder from './menu-order'
 
 export default function GuestMenu() {
-  return <div>GuestMenu</div>
+  return (
+    <div className="max-w-[400px] mx-auto space-y-4">
+      <h1 className="text-center text-xl font-bold">🍕 Menu quán</h1>
+      <MenuOrder />
+    </div>
+  )
 }
